b952ba68720f0f1a70f3a07beade314d572d8e90
Emanuel Brites <emabrio@gmail.com>
2021-11-04 20:32:20 -0300

    Obtención de datos

--- js/get.js
+++ js/get.js
@@ -1,1 +1,13 @@
 "uses strict";
+let boton = document.getElementById('btn-get');
+boton.addEventListener('click', getDatos);
+const url = "https://60c2aab9917002001739d577.mockapi.io/bat/voluntarios";
+async function getDatos() {
+    try {
+        let res = await fetch(url);
+        let json = await res.json();
+        console.log(json);
+    } catch (error) {
+        console.log(error);
+    }
+}
