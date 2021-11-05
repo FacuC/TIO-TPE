ad5aca1fb9dbca3f835fe44f6001d0b7e974bdb6
Facundo Cristobal <facucristobal@gmail.com>
2021-11-05 16:51:20 -0300

    agregada la funcion editar

---
+++ js/put.js
@@ -0,0 +1,42 @@
+"uses strict";
+const url = "https://60c2aab9917002001739d577.mockapi.io/bat/voluntarios";
+async function editar(id) {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let telefono = document.getElementById("telefono").value;
+    let mail = document.getElementById("mail").value;
+    let area = document.getElementById("area").value;
+    let turno = document.getElementById("turno").value;
+
+    if (
+        nombre == "" ||
+        apellido == "" ||
+        telefono == "" ||
+        mail == "" ||
+        area == "" ||
+        turno == ""
+    ) {
+        console.log("uno de los campos estaba vacio");
+    } else {
+        let nuevoContenido = {
+            nombre: nombre,
+            apellido: apellido,
+            telefono: telefono,
+            mail: mail,
+            area: area,
+            turno: turno,
+        };
+        try {
+            let res = await fetch(url + "/" + id, {
+                method: "PUT",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify(nuevoContenido),
+            });
+            if (res.ok) {
+                console.log("Modificado!");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
